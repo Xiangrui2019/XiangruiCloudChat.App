366e5ea32956ff23c85ec2bcda681d5de2fa472c
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-03-18 19:22:08 +0800

    Zone

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  server: 'http://localhost:5000',
-  applicationServerKey: 'BJ3P9MnJVCofyrEq2KaE-e_lMlhmZp1ofUpXzJgINsQQHGbMjN59WoF6XmvTivRbOmMM0PFhXMG8k5qU5TtT0AM'
-};
