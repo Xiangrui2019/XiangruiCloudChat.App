f0b1da74f60ba5e151c6de636e17d5189a9c040c
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-03-21 20:00:05 +0800

    change web.config to publish

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  server: 'https://xiangruichatapp.co:5400',
+  applicationServerKey: 'BJ3P9MnJVCofyrEq2KaE-e_lMlhmZp1ofUpXzJgINsQQHGbMjN59WoF6XmvTivRbOmMM0PFhXMG8k5qU5TtT0AM'
+};
