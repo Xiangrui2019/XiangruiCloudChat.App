16278ed353234904b45cc8270a159690d729294b
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-03-24 18:44:32 +0800

    Update News

--- src/app/Controllers/news.component.ts
+++ src/app/Controllers/news.component.ts
@@ -1,5 +1,5 @@
+import { ApiService } from './../Services/ApiService';
 import { Component, OnInit } from '@angular/core';
-// import { CheckService } from '../Services/CheckService';
 import { HeaderService } from '../Services/HeaderService';
 
 @Component({
@@ -7,13 +7,17 @@ import { HeaderService } from '../Services/HeaderService';
     styleUrls: [
         '../Styles/news.css',
         '../Styles/menu.css',
-        '../Styles/button.css'
+        '../Styles/button.css',
+        '../Styles/add-friend.css'
     ]
 })
 
 export class NewsComponent implements OnInit {
+    public news = [];
+
     constructor(
-        private headerService: HeaderService
+        private headerService: HeaderService,
+        private apiService: ApiService
     ) {
         this.headerService.title = '新闻';
         this.headerService.returnButton = true;
@@ -21,6 +25,16 @@ export class NewsComponent implements OnInit {
         this.headerService.shadow = false;
     }
 
+    public updateNews() {
+        this.apiService.Get('/News/GetNews').subscribe((data) => {
+            for (const newp of data['data']) {
+                this.news.push(newp);
+                window.scrollTo(0, document.documentElement.clientHeight);
+            }
+        });
+    }
+
     ngOnInit(): void {
+        this.updateNews();
     }
 }
